163ff70d5221fe7c2ebca2dd1220aa8c8ccccaef
Furkan394 <furkanncakiroglu@gmail.com>
2022-10-23 17:15:55 +0300

    added first migration

---
+++ Infrastructure/ECommerceAPI.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ECommerceAPI.Persistence.Contexts;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace ECommerceAPI.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ECommerceAPIDbContext>(options => 
+                                            options.UseNpgsql(configuration.GetConnectionString("PostgreSQL")));
+        }
+    }
+}
